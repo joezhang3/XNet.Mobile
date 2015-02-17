a958f3897bc09500be16a5957f3816c3c3deee37
joezhang3 <yanbei0391@aliyun.com>
2015-02-17 12:53:00 +0800

    Update

---
+++ XNet.DataProvider/Entity/Activity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace XNet.DataProvider
+{
+	public class Activity
+	{
+		public Activity ()
+		{
+
+		}
+
+		public string Name {get;set;}
+		public int AvailableSpots{ get; set;}
+		public double Price{ get; set;}
+		public string Location {get;set;}
+		public int Id {get;set;}
+	}
+}
+
---
+++ XNet.Mobile/HomePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XNet.DataProvider;
+
+namespace XNet.Mobile
+{
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+			listView.ItemsSource = ActivityProvider.GetActivities();
+		}
+	}
+}
+
